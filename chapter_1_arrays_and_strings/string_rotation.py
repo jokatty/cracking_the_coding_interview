dcd3ec18064a42ed1981f0272eb65c3eaa560c0c
jokatty <jyotikattani@gmail.com>
2024-04-16 12:11:20 +0200

    add string rotation

---
+++ chapter_1_arrays_and_strings/string_rotation.py
@@ -0,0 +1,13 @@
+def is_substring(string, sub):
+    return string.find(sub) != -1
+
+
+def is_rotation(s1, s2):
+    if len(s1) == len(s2) != 0:
+        return is_substring(s1+s1, s2)
+    else:
+        return False
+
+print(is_rotation('waterbottle', 'erbottlewat'))
+print(is_rotation('foo', 'bar'))
+print(is_rotation('foo', 'foofoo'))
